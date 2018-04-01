2a9d95020def6dced4e740ac56f25a55a8e1d600
Aruna <aruna.thungathurthi@gmail.com>
2018-04-01 08:44:29 -0700

    added images

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'animals/index'
+  get "/animals", to: "animals#index", as:"animals"
+  get "animals/new", to: 'animals#new', as: 'new_animal'
+  post '/animals',to: 'animals#create'
+  get '/animals/:id/edit', to: 'animals#edit#', as: 'edit_task'
 
-  get 'animals/show'
+  get "/animals/:id",to: "animals#show", as:"show"
+  patch "/animals/:id", to: "animals#update"
+  delete "/animals/:id", to: "animals#destroy"
 
-  get 'animals/new'
 
-  get 'animals/create'
 
-  get 'animals/edit'
 
-  get 'animals/update'
-
-  get 'animals/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
