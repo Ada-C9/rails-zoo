cad86cbb6e87fae19ffd943124b2e62289d10e5a
0velez0 <mariaivelez@gmail.com>
2018-04-30 21:16:09 -0700

    create routes and view template files for index, new and show; create Animal model

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,22 @@
 class AnimalsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/animals/new', to: 'animals#new'
+  get '/animals', to: 'animals#index'
+  get '/animals/:id', to: 'animals#show', as: 'animal'
+
 end
