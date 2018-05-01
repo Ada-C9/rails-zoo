5f2ba2ed9d2a90442ca5002a2a6e3f6cc1b12e21
0velez0 <mariaivelez@gmail.com>
2018-05-01 01:05:12 -0700

    add link_to from index to new view; update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/animals/new', to: 'animals#new'
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
   post '/animals', to: 'animals#create'
 
   get '/animals', to: 'animals#index'
